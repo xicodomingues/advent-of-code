1568715f1ba4e5185590934ee302cdda429ab81e
Francisco Domingues <xico.domingues@gmail.com>
2023-03-09 13:37:11 +0100

    Create comon solve for day 6 2015

--- src/year2015/day6.rs
+++ src/year2015/day6.rs
@@ -68,44 +68,52 @@ trait ExecInstruction<T> {
 
 impl<T> ExecInstruction<T> for MyGrid<T> {
     fn exec(&mut self, coords: Coords, make_change: impl Fn(&T) -> T) {
-        (coords.top.y..=coords.bottom.y).cartesian_product(coords.top.x..=coords.bottom.x)
+        (coords.top.y..=coords.bottom.y)
+            .cartesian_product(coords.top.x..=coords.bottom.x)
             .for_each(|p| self[p] = make_change(&self[p]));
     }
 }
 
-pub fn part1(input: &str) -> usize {
-    let mut grid: MyGrid<bool> = MyGrid(Grid::new(1000, 1000));
+fn solve<T>(input: &str, make_change: impl Fn(Instruction) -> Box<dyn Fn(&T) -> T>) -> MyGrid<T>
+where
+    T: Default,
+{
+    let mut grid: MyGrid<T> = MyGrid(Grid::new(1000, 1000));
     input
         .lines()
         .map(|line| (Instruction::from_str(line), Coords::from_str(line)))
         .for_each(|info| match info {
-            (Ok(inst), Ok(coords)) => grid.exec(coords, |x| {
-                match inst {
-                    Instruction::TurnOff => false,
-                    Instruction::TurnOn => true,
-                    Instruction::Toggle => !x,
-                }
-            }),
+            (Ok(inst), Ok(coords)) => grid.exec(coords, &make_change(inst)),
             _ => panic!("Faulty instruction"),
         });
+    grid
+}
+
+pub fn part1(input: &str) -> usize {
+    let grid = solve(input, |inst| {
+        Box::new(move |x| match inst {
+            Instruction::TurnOff => false,
+            Instruction::TurnOn => true,
+            Instruction::Toggle => !x,
+        })
+    });
     grid.iter().filter(|x| **x).count()
 }
 
 pub fn part2(input: &str) -> usize {
-    let mut grid: MyGrid<usize> = MyGrid(Grid::new(1000, 1000));
-    input
-        .lines()
-        .map(|line| (Instruction::from_str(line), Coords::from_str(line)))
-        .for_each(|info| match info {
-            (Ok(inst), Ok(coords)) => grid.exec(coords, |x| {
-                match inst {
-                    Instruction::TurnOff => if x > &1 {x - 1} else {0},
-                    Instruction::TurnOn => x + 1,
-                    Instruction::Toggle => x + 2,
+    let grid = solve(input, |inst| {
+        Box::new(move |x| match inst {
+            Instruction::TurnOff => {
+                if x > &1 {
+                    x - 1
+                } else {
+                    0
                 }
-            }),
-            _ => panic!("Faulty instruction"),
-        });
+            }
+            Instruction::TurnOn => x + 1,
+            Instruction::Toggle => x + 2,
+        })
+    });
     grid.iter().sum()
 }
 
@@ -113,5 +121,5 @@ pub fn part2(input: &str) -> usize {
 fn test() {
     assert_eq!(part2("turn on 0,0 through 0,0"), 1);
     assert_eq!(part2("toggle 0,0 through 999,999"), 2_000_000);
-    crate::test_2015!(6, 1_000_000 - 1000 - 4, 1_000_000 + 2*1000 - 4)
+    crate::test_2015!(6, 1_000_000 - 1000 - 4, 1_000_000 + 2 * 1000 - 4)
 }
