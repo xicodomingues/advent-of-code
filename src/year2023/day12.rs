dab440d24c106038856e429981babbd3577fcd3f
Francisco <xico.domingues@gmail.com>
2023-12-14 17:36:11 +0100

    --wip-- [skip ci]

--- src/year2023/day12.rs
+++ src/year2023/day12.rs
@@ -3,75 +3,35 @@ use regex::Regex;
 struct HotSpring {
     record: Vec<u8>,
     current: String,
-    correct_match: Regex,
-    maybe_match: Regex,
 }
 
 impl HotSpring {
     fn parse(line: &str) -> Self {
-        fn exact_match(input: &[u8]) -> Regex {
-            let mut regex = r"^\.*".to_string();
-            for x in input {
-                regex.push_str(&format!("#{{{x}}}\\.+"))
-            }
-            regex.pop();
-            regex.push_str("*$");
-            Regex::new(&regex).unwrap()
-        }
-
-        fn maybe_match(input: &[u8]) -> Regex {
-            let mut regex = r"^[\.?]*".to_string();
-            for x in input {
-                regex.push_str(&format!("[#?]{{{x}}}[\\.?]+"))
-            }
-            regex.pop();
-            regex.push_str("*$");
-            Regex::new(&regex).unwrap()
-        }
-
         let (state, rec) = line.trim().split_once(' ').unwrap();
         let record: Vec<u8> = rec.split(',').map(|x| x.parse().unwrap()).collect();
         Self {
             current: state.to_owned(),
-            correct_match: exact_match(&record),
-            maybe_match: maybe_match(&record),
             record,
         }
     }
 
-    fn is_match(&self, test: &str) -> bool {
-        self.correct_match.is_match(test)
-    }
-
-    fn can_match(&self, test: &str) -> bool {
-        self.maybe_match.is_match(test)
-    }
-        
-    fn count_matches_rec(&self, input: &mut String, index: usize) -> usize {
-        assert!(input.bytes().take(index).all(|x| x != b'?'));
-        if let Some(next) = input.bytes().skip(index).position(|x| x == b'?') {
-            let idx = next + index;
-            let s_bytes: &mut [u8] = unsafe { input.as_bytes_mut() };
-            s_bytes[idx] = b'.';
-            let temp = if self.can_match(&input) {
-                self.count_matches_rec(input, idx + 1)
-            } else {
-                0
-            };
-            let s_bytes: &mut [u8] = unsafe { input.as_bytes_mut() };
-            s_bytes[idx] = b'#';
-            temp + if self.can_match(&input) {
-                self.count_matches_rec(input, idx + 1)
-            } else {
-                0
+    // add memo later
+    fn count_matches_rec(&self, input: &str, target: Vec<u8>, mut total_pounds: isize) -> usize {
+        let mut x = input.trim_start_matches('.');
+        let mut cpy = target.clone();
+        let first = x.bytes().next().unwrap();
+        // check case where first on target is zero
+        match first {
+            b'#' => {
+                total_pounds -= 1;
+                cpy[0] -= 1;
             }
-        } else {
-            self.is_match(&input).into()
         }
+        0
     }
 
     fn count_matches(&self) -> usize {
-        self.count_matches_rec(&mut self.current.to_owned(), 0)
+        0
     }
 }
 
@@ -82,21 +42,6 @@ fn test_spring() {
     }
 
     let hs = h(".#......###.#..######.. 1,3,1,6");
-
-    assert!(hs.is_match(&".#......###.#..######.."));
-    assert!(hs.is_match(&"#.###.#.######"));
-    assert!(!hs.is_match(&"#..##.#.######"));
-
-    assert!(hs.can_match(&".#......###.#..######.."));
-    assert!(hs.can_match(&"#.###.#.######"));
-    assert!(!hs.can_match(&"#..##.#.######"));
-
-    assert!(h("???.### 1,1,3").can_match("???.###"));
-    assert!(h(".??..??...?##. 1,1,3").can_match(".??..??...?##."));
-    assert!(h("?#?#?#?#?#?#?#? 1,3,1,6").can_match("?#?#?#?#?#?#?#?"));
-    assert!(h("????.#...#... 4,1,1").can_match("????.#...#..."));
-    assert!(h("????.######..#####. 1,6,5").can_match("????.######..#####."));
-    assert!(h("?###???????? 3,2,1").can_match("?###????????"));
 
     assert_eq!(h("???.### 1,1,3").count_matches(), 1);
     assert_eq!(h("?###???????? 3,2,1").count_matches(), 10);
@@ -110,8 +55,12 @@ pub fn part1(input: &str) -> usize {
         .sum()
 }
 
-pub fn part2(_input: &str) -> usize {
-    0
+pub fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .map(HotSpring::parse)
+        .map(|hs| hs.count_matches())
+        .sum()
 }
 
 #[test]
